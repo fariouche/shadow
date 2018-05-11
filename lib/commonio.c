3307ff60f69cb1c66a0d7f3a0bd660302a46f596
farid <farid.benamrouche@sagemcom.com>
2018-05-11 12:14:29 +0200

    use lckpwdf only if no prefix

--- lib/commonio.c
+++ lib/commonio.c
@@ -412,11 +412,43 @@ cleanup_ENOMEM:
 	return err;
 }
 
+static int _commonio_lock(struct commonio_db *db)
+{
+	int i;
+
+	/*
+	 * lckpwdf() not used - do it the old way.
+	 */
+#ifndef LOCK_TRIES
+#define LOCK_TRIES 15
+#endif
+
+#ifndef LOCK_SLEEP
+#define LOCK_SLEEP 1
+#endif
+	for (i = 0; i < LOCK_TRIES; i++) {
+		if (i > 0) {
+			sleep (LOCK_SLEEP);	/* delay between retries */
+		}
+		if (commonio_lock_nowait (db, i==LOCK_TRIES-1) != 0) {
+			return 1;	/* success */
+		}
+		/* no unnecessary retries on "permission denied" errors */
+		if (geteuid () != 0) {
+			(void) fprintf (stderr, "%s: Permission denied.\n",
+			                Prog);
+			return 0;
+		}
+	}
+	return 0;		/* failure */
+}
 
 int commonio_lock (struct commonio_db *db)
 {
-/*#ifdef HAVE_LCKPWDF*/ /* not compatible with prefix option*/
-#if 0
+#ifdef HAVE_LCKPWDF /* not compatible with prefix option*/
+
+	if (strncmp(db->filename, "/etc/", 5))
+		return _commonio_lock (db);
 	/*
 	 * only if the system libc has a real lckpwdf() - the one from
 	 * lockpw.c calls us and would cause infinite recursion!
@@ -445,33 +477,7 @@ int commonio_lock (struct commonio_db *db)
 	ulckpwdf ();
 	return 0;		/* failure */
 #else				/* !HAVE_LCKPWDF */
-	int i;
-
-	/*
-	 * lckpwdf() not used - do it the old way.
-	 */
-#ifndef LOCK_TRIES
-#define LOCK_TRIES 15
-#endif
-
-#ifndef LOCK_SLEEP
-#define LOCK_SLEEP 1
-#endif
-	for (i = 0; i < LOCK_TRIES; i++) {
-		if (i > 0) {
-			sleep (LOCK_SLEEP);	/* delay between retries */
-		}
-		if (commonio_lock_nowait (db, i==LOCK_TRIES-1) != 0) {
-			return 1;	/* success */
-		}
-		/* no unnecessary retries on "permission denied" errors */
-		if (geteuid () != 0) {
-			(void) fprintf (stderr, "%s: Permission denied.\n",
-			                Prog);
-			return 0;
-		}
-	}
-	return 0;		/* failure */
+	return _commonio_lock (db);
 #endif				/* !HAVE_LCKPWDF */
 }
 
